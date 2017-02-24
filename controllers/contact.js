c597ebbe764116c6c0b941f769706215a40a1f23
Colleen Dunlap <colleen.dunlap@marquette.edu>
2017-02-24 20:51:04 +0000

    fixed error handling in contact form

--- controllers/contact.js
+++ controllers/contact.js
@@ -23,9 +23,9 @@ exports.getContact = (req, res) => {
  * Send a contact form via Nodemailer.
  */
 exports.postContact = (req, res) => {
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('message', 'Message cannot be blank').notEmpty();
+  req.assert('name', 'Event cannot be blank').notEmpty();
+  req.assert('email', 'Must add a location').notEmpty();
+  req.assert('message', 'Description cannot be blank').notEmpty();
 
   const errors = req.validationErrors();
 
