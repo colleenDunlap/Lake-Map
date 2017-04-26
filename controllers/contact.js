fce1c179b5133bf73b6a6a08cdae7244f819efe2
= <davidjosephvitale@gmail.com>
2017-04-26 16:34:11 +0000

    Adding pins adds to the database

--- controllers/contact.js
+++ controllers/contact.js
@@ -23,6 +23,8 @@ exports.getContact = (req, res) => {
  * Send a contact form via Nodemailer.
  */
 exports.postContact = (req, res) => {
+
+
   
   console.log(req.body);
   req.assert('name', 'Event cannot be blank').notEmpty();
@@ -38,11 +40,24 @@ exports.postContact = (req, res) => {
     return res.redirect('/contact');
   }
   const newPin = {
-    pin_name: '${req.body.name}',
-    latitude: '${req.body.latitude}',
-    longitude: '${req.body.longitude}'
-    
-  };
+    pin_name: req.body.name,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    description: req.body.message
+    };
+
+  var MongoClient = require('mongodb').MongoClient
+        , async = require('async')
+
+        MongoClient.connect('mongodb://localhost:27017/lakemapv02', function(err, db) {
+                db.collection('pins', function(err, coll){
+                        console.log("Inserting this pin!")
+			coll.insert(newPin)
+                        });
+                });
+
+  res.redirect('/contact')
+//               res.end(JSON.stringify(data));
   
   /*
   const mailOptions = {
@@ -53,12 +68,12 @@ exports.postContact = (req, res) => {
   };
 */
 
-  transporter.sendMail(newPin, (err) => {
+  /*transporter.sendMail(newPin, (err) => {
     if (err) {
       req.flash('errors', { msg: err.message });
       return res.redirect('/contact');
     }
     req.flash('success', { msg: 'Email has been sent successfully!' });
     res.redirect('/contact');
-  });
+  });*/
 };
